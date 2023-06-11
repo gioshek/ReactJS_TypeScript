675c2c609e9cafc7e9f01b24190e351bb69b28c1
Giorgi <giorgikutaisi15@gmail.com>
2023-06-11 19:57:46 +0300

    second commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quibusdam voluptatem similique! Asperiores eos nostrum deleniti cum natus, explicabo, earum facere quisquam libero molestias maiores eaque perspiciatis inventore minima totam modi beatae vel! Quaerat dignissimos cumque repellendus nostrum, quo enim repellat natus nobis aperiam sapiente esse beatae sed, eum ipsum.</div>
+    )
+}
